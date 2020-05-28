3b54e98b0522b0912025f8939c8db74a03342870
AndersonKardek <kardek.anderson@gmail.com>
2020-05-28 15:52:28 -0400

    n deu certo

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
-const log = require('electron-log');
-
-// configure logging
-autoUpdater.logger = log;
-autoUpdater.logger.transports.file.level = 'info';
-log.info('App starting...');
 
 let mainWindow;
 
@@ -27,68 +21,32 @@ function createWindow() {
     });
 }
 
+app.on('ready', () => {
+    createWindow();
+});
 
-
-// app.on('ready', () => {
-//     createWindow();
-// });
-
-// app.on('window-all-closed', function () {
-//     if (process.platform !== 'darwin') {
-//         app.quit();
-//     }
-// });
-
-// app.on('activate', function () {
-//     if (mainWindow === null) {
-//         createWindow();
-//     }
-// });
-
-// ipcMain.on('app_version', (event) => {
-//     event.sender.send('app_version', { version: app.getVersion() });
-// });
-
-// autoUpdater.on('update-available', () => {
-//     mainWindow.webContents.send('update_available');
-// });
-// autoUpdater.on('update-downloaded', () => {
-//     mainWindow.webContents.send('update_downloaded');
-// });
-// ipcMain.on('restart_app', () => {
-//     autoUpdater.quitAndInstall();
-// });
-const sendStatusToWindow = (text) => {
-    log.info(text);
-    if (mainWindow) {
-        mainWindow.webContents.send('message', text);
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
-};
-
-autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
-});
-autoUpdater.on('update-available', info => {
-    sendStatusToWindow('Update available.');
-});
-autoUpdater.on('update-not-available', info => {
-    sendStatusToWindow('Update not available.');
-});
-autoUpdater.on('error', err => {
-    sendStatusToWindow(`Error in auto-updater: ${err.toString()}`);
-});
-autoUpdater.on('download-progress', progressObj => {
-    sendStatusToWindow(
-        `Download speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.percent}% (${progressObj.transferred} + '/' + ${progressObj.total} + )`
-    );
-});
-autoUpdater.on('update-downloaded', info => {
-    sendStatusToWindow('Update downloaded; will install now');
 });
 
-autoUpdater.on('update-downloaded', info => {
-    // Wait 5 seconds, then quit and install
-    // In your application, you don't need to wait 500 ms.
-    // You could call autoUpdater.quitAndInstall(); immediately
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+ipcMain.on('app_version', (event) => {
+    event.sender.send('app_version', { version: app.getVersion() });
+});
+
+autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+});
+ipcMain.on('restart_app', () => {
     autoUpdater.quitAndInstall();
 });
