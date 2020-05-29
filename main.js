01f1e5826bb088d7549ee7743f0da2c5f9a954fa
AndersonKardek <kardek.anderson@gmail.com>
2020-05-29 09:03:24 -0400

    teste2

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow() {
         mainWindow = null;
     });
 
-    mainWindow.once('ready-to-show', () => {
+    mainWindow.once('did-finish-load', () => {
         autoUpdater.checkForUpdatesAndNotify();
     });
 }
